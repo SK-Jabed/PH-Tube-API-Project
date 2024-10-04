ea0fe54c4df7b1ccb39dc432da0d5ccdbf5e1f9a
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-04 23:10:32 +0600

    All Files Completed of This Project

---
+++ summary.js
@@ -0,0 +1,14 @@
+/*
+ * ----- PROJECT SUMMARY -----
+ * 01. Load and display data initially
+ * 02. onClick load and display data
+ * 03. Complex object accessing
+ * 04. Toggle active class functionality
+ * 05. Search functionality
+ * 06. Load and display single data
+ * 07. Display data in a modal
+ * 08. Load and display single category data
+ * 09. Conditional rendering if array don't have any data
+ * 10. Conditional Rendering:: if a value empty
+ * 11. Conditional Rendering:: if a value length is zero
+*/
